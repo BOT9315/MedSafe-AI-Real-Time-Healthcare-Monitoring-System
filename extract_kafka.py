9580e3891e09159b5ad8216177b86af5ea82fd97
Ankush <akrk053@gmail.com>
2026-04-05 12:57:50 +0530

    Update extract_kafka.py

--- extract_kafka.py
+++ extract_kafka.py
@@ -1,5 +1,4 @@
 import tarfile
-
 # Replace this path with your uploaded file
 tgz_file = r"C:\kafka_2.13-4.2.0\kafka_2.13-4.2.0\site-docs\kafka_2.13-4.2.0-site-docs.tgz"
 extract_path = r"C:\kafka_extracted"
